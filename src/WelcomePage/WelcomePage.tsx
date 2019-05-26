9252ead94e8365e266653a553856731bae1a3528
Gabriel Restori Soares <g.restorisoares@criteo.com>
2019-05-26 19:59:01 +0200

    Added catch phrase and logo to welcome page

--- src/WelcomePage/WelcomePage.tsx
+++ src/WelcomePage/WelcomePage.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import BackgroundClouds from './BackgroundClouds';
+
+import { Row, Col, Button } from 'react-bootstrap';
+import WelcomePhrase from './WelcomePhrase';
+
+import logo from './compass.svg';
 import './WelcomePage.scss';
 
 const WelcomePage: React.FC = () => {
   return (
     <div className="WelcomePage">
-      <BackgroundClouds />
+      <Row className="h-100">
+        <Col xs={{span: 12, order: 2}} md={{ span: 6, order: 1 }}>
+          <WelcomePhrase />
+          <Button size="lg" variant="primary"><b>Sim!</b></Button>
+        </Col>
+        <Col xs={{span: 12, order: 1}} md={{ span: 6, order: 2 }}>
+          <Row className="h-100">
+            <Col xs={{ span: 8, offset: 2}} className="my-auto">
+              <img src={logo} alt="Logo" className="WelcomePageLogo" />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 }
---
+++ src/WelcomePage/WelcomePhrase.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './WelcomePhrase.scss';
+
+const WelcomePhrase: React.FC = () => {
+    return (
+        <div className="WelcomePhrase">
+            <h1 className="WelcomePhraseIntroduction">
+                Bem-vindo, eu sou o <em>Anemoi.</em> 
+                <br />
+                Expert em <em>viagens.</em>
+                <br />
+            </h1>
+            <h1 className="WelcomePhraseCallToAction">
+                Possor ajudar a planejar a sua?
+            </h1>
+        </div>
+    );
+}
+
+export default WelcomePhrase;
