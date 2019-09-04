e56ff745b5de5142f2cadf2c5c35fe5ffa86e376
Gabriel Restori Soares <g.restorisoares@criteo.com>
2019-09-04 19:31:47 +0200

    Fixed key warning in travel plan result

--- src/TravelPlannerWorkflow/TravelPlanResult/TravelPlanResult.tsx
+++ src/TravelPlannerWorkflow/TravelPlanResult/TravelPlanResult.tsx
@@ -1,6 +1,6 @@
 import './TravelPlanResult.scss';
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import TravelPlanResult from '../../Services/AnemoiServices/TravelPlanResult';
 
 import AirplaneRouteResult from './AirplaneRouteResult';
@@ -15,7 +15,12 @@ const TravelPlanResultComponent: React.FC<TravelPlanResultProps> = props => {
     <div>
       <h4><em>Melhor rota encontrada!</em></h4>
       <h4>Aqui está o seu plano de viagem:</h4>
-      {props.result.routes.map(route => <AirplaneRouteResult route={route} />)}
+      {props.result.routes.map(route => (
+        <Fragment key={`${route.source}-${route.destination}`}>
+          <AirplaneRouteResult route={route} />
+        </Fragment>
+        )
+      )}
       <h4><em>Preço total:</em> R$ {props.result.totalPrice.toFixed(2).replace('.', ',')}</h4>
       <Button block size="lg" variant="success" onClick={() => window.open(props.result.deepLink, '_blank')}>
         <b>Clique aqui para reservar</b>
