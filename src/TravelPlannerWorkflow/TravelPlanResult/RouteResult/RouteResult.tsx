861c59eb917a9e7bb12a59581ed84c0a20170be7
Gabriel Restori Soares <g.restorisoares@criteo.com>
2019-09-14 08:00:14 +0200

    Finished basic styling of TravelPlanResult

--- src/TravelPlannerWorkflow/TravelPlanResult/RouteResult/RouteResult.tsx
+++ src/TravelPlannerWorkflow/TravelPlanResult/RouteResult/RouteResult.tsx
@@ -1,9 +1,12 @@
 import './RouteResult.scss'
 
 import React from 'react';
-import { Route } from '../../../Services/AnemoiServices/TravelPlanResult';
 import { Row, Col } from 'react-bootstrap';
+
+import { Route } from '../../../Services/AnemoiServices/TravelPlanResult';
 import FlagIcon from '../../../Shared/FlagIcon';
+
+import AirPlaneTravelIcon from './airplane-travel-icon.svg';
 
 interface RouteResultProps {
   route: Route;
@@ -23,15 +26,17 @@ const RouteResult: React.FC<RouteResultProps> = props => {
 
   return (
     <Row className="RouteResult">
-      <Col xs={5} className="RouteCity">
+      <Col xs={4} className="RouteCity">
         <FlagIcon className="FlagIcon" size="3x" squared code={props.route.source.countryCode.toLowerCase()} />
         <br />
         <span className="CityLabel">{props.route.source.cityName}, {props.route.source.countryName}</span>
         <br />
         <span className="DateLabel">{new Date(props.route.startTime).toLocaleDateString(locale, dateStringOptions)}</span>
       </Col>
-      <Col xs={2} className="RouteSign">-></Col>
-      <Col xs={5} className="RouteCity">
+      <Col xs={4} className="RouteSign my-auto">
+        <img src={AirPlaneTravelIcon} alt="Airplane Travel" />
+      </Col>
+      <Col xs={4} className="RouteCity">
         <FlagIcon className="FlagIcon" size="3x" squared code={props.route.destination.countryCode.toLowerCase()} />
         <br />
         <span className="CityLabel">{props.route.destination.cityName}, {props.route.destination.countryName}</span>
