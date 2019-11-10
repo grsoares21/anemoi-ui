423eaa4e0e02e17a0d4995b62dfa6125f92d6e4e
Gabriel Restori Soares <soares-21@hotmail.com>
2019-11-10 09:46:09 +0100

    Fixed test name for WelcomePhrase component

--- src/WelcomePage/WelcomePhrase/WelcomePhrase.test.tsx
+++ src/WelcomePage/WelcomePhrase/WelcomePhrase.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import WelcomePhrase from './WelcomePhrase';
 
-describe('WelcomePage component', () => {
+describe('WelcomePhrase component', () => {
   afterEach(() => {
     cleanup();
   });
