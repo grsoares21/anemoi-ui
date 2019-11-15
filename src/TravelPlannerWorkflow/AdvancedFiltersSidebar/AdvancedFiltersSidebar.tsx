4734d7128946a374461a97eb70c01433c4dcb785
Gabriel Restori Soares <soares-21@hotmail.com>
2019-11-15 20:00:57 +0100

    Added media query based docking for filters sidebar

--- src/TravelPlannerWorkflow/AdvancedFiltersSidebar/AdvancedFiltersSidebar.tsx
+++ src/TravelPlannerWorkflow/AdvancedFiltersSidebar/AdvancedFiltersSidebar.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
+import useMedia from 'use-media';
 //import { useTranslation } from 'react-i18next';
 import Sidebar from 'react-sidebar';
 
 const AdvancedFiltersSidebar: React.FC = () => {
   //const { t } = useTranslation();
   const [open, setOpen] = useState(false);
+  const isWide = useMedia({ minWidth: 800 });
+
+  const sidebarStyles = {
+    sidebar: { background: 'white' },
+    content: { zIndex: '3' }
+  };
 
   const sidebarContent = <b>Sidebar content</b>;
   return (
-    <Sidebar sidebar={sidebarContent} open={open} onSetOpen={setOpen} styles={{ sidebar: { background: 'white' } }}>
-      <button onClick={() => setOpen(true)}>Open sidebar</button>
+    <Sidebar sidebar={sidebarContent} open={open} onSetOpen={setOpen} styles={sidebarStyles} docked={isWide && open}>
+      <button onClick={() => setOpen(!open)}>Open sidebar</button>
     </Sidebar>
   );
 };
