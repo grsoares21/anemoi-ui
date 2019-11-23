86d1c176354ec740f86278dc8e7b02f54d3bfa3d
Gabriel Restori Soares <soares-21@hotmail.com>
2019-11-23 15:19:10 +0100

    Created TravelPlannerWorkflow Context

--- src/TravelPlannerWorkflow/TravelPlannerWorkflow.d.ts
+++ src/TravelPlannerWorkflow/TravelPlannerWorkflow.d.ts
@@ -1,31 +1,32 @@
 import { City } from '../Services/LocationServices';
 import TravelPlanResult from '../Services/AnemoiServices/TravelPlanResult';
+
 export type CityStayPeriod = {
-  city: City,
-  minDays: number,
-  maxDays: number
+  city: City;
+  minDays: number;
+  maxDays: number;
 };
 
 export type DateRange = {
-  startDate: Date | null,
-  endDate: Date | null
+  startDate: Date | null;
+  endDate: Date | null;
 };
 
-export type State = {
-  departureCities: City[],
-  arrivalCities: City[],
-  visitingCities: CityStayPeriod[],
-  departureDateRange: DateRange,
-  arrivalDateRange: DateRange,
-  travelPlanResult?: TravelPlanResult
+export type TravelPlannerWorkflowState = {
+  departureCities: City[];
+  arrivalCities: City[];
+  visitingCities: CityStayPeriod[];
+  departureDateRange: DateRange;
+  arrivalDateRange: DateRange;
+  travelPlanResult?: TravelPlanResult;
 };
 
-export type Action =
-  | { type: 'setDepartureCities', cities: City[] }
-  | { type: 'setArrivalCities', cities: City[] }
-  | { type: 'setVisitingCities', cities: CityStayPeriod[] }
-  | { type: 'setDateRanges', dateRanges: { departureDateRange: DateRange, arrivalDateRange: DateRange } }
-  | { type: 'setTravelPlanResult', result: TravelPlanResult };
+export type TravelPlannerWorkflowAction =
+  | { type: 'setDepartureCities'; cities: City[] }
+  | { type: 'setArrivalCities'; cities: City[] }
+  | { type: 'setVisitingCities'; cities: CityStayPeriod[] }
+  | { type: 'setDateRanges'; dateRanges: { departureDateRange: DateRange; arrivalDateRange: DateRange } }
+  | { type: 'setTravelPlanResult'; result: TravelPlanResult };
 
 export enum WorkflowSection {
   Beginning = 0,
@@ -36,4 +37,4 @@ export enum WorkflowSection {
   CalculateTravelPlan = 5,
   CalculatingTravelPlan = 6,
   TravelPlanResult = 7
-};
+}
