f2d5f4892347c6bb427c7c51be9074251384ab23
Gabriel Restori Soares <soares-21@hotmail.com>
2020-02-24 12:52:48 +0100

    Created custom handle for RangeSlider

--- src/Shared/RangeSlider/RangeSlider.tsx
+++ src/Shared/RangeSlider/RangeSlider.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rheostat from 'rheostat';
+import './RangeSlider.scss';
 
 interface RangeSliderProps {
   snap: boolean;
@@ -9,9 +10,19 @@ interface RangeSliderProps {
   onChange: (values: number[]) => void;
 }
 
+const SliderHandle: React.FC<{ handleRef: any; 'aria-valuenow': any }> = ({ handleRef, ...passProps }) => {
+  const value = passProps['aria-valuenow'];
+  return (
+    <div className="RangeSliderHandle" ref={handleRef} {...passProps}>
+      {value}
+    </div>
+  );
+};
+
 const RangeSlider: React.FC<RangeSliderProps> = props => {
   return (
     <Rheostat
+      handle={SliderHandle}
       snap={props.snap}
       min={props.min}
       max={props.max}
