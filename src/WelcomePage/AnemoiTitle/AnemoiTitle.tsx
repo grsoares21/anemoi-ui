499de4a557287d98920971cf545a145184ad7e11
Gabriel Restori Soares <soares-21@hotmail.com>
2020-10-05 21:31:01 +0200

    Published dark theme

--- src/WelcomePage/AnemoiTitle/AnemoiTitle.tsx
+++ src/WelcomePage/AnemoiTitle/AnemoiTitle.tsx
@@ -6,22 +6,24 @@ const Title = posed.h3({
   exit: { opacity: 0 },
   enter: {
     opacity: 1,
-    transition: { ease: 'linear', duration: 200 }
-  }
+    transition: { ease: 'linear', duration: 200 },
+  },
 });
 
 interface AnemoiTitleProps {
   isVisible: boolean;
 }
 
-const AnemoiTitle: React.FC<AnemoiTitleProps> = props => {
+const AnemoiTitle: React.FC<AnemoiTitleProps> = (props) => {
   return (
     <PoseGroup>
       {props.isVisible && (
         <Title key="anemoiTitle">
-          <span onClick={() => window.location.reload()} data-testid="anemoi-title">
-            Anemoi
-          </span>
+          <a>
+            <span onClick={() => window.location.reload()} data-testid="anemoi-title">
+              Anemoi
+            </span>
+          </a>
         </Title>
       )}
     </PoseGroup>
