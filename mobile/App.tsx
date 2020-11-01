58edcd3b6027fb75c59b5d2398050d5a190b07f0
Gabriel Restori Soares <grsoares21@gmail.com>
2020-11-01 18:42:43 +0100

    Bootstrapped main page on mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Anemoi.app soon available on mobile!</Text>
       <StatusBar style="auto" />
     </View>
   );
