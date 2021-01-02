008e6fb70a00ebe17fcc7fc69a1b5258873e3c11
Gabriel Restori Soares <grsoares21@gmail.com>
2021-01-02 17:25:32 +0100

    Started implementing color mask effect with masked view

--- mobile/TravelPlannerWorkflow/CitySelectionWorkflow/CitySelectionWorkflow.tsx
+++ mobile/TravelPlannerWorkflow/CitySelectionWorkflow/CitySelectionWorkflow.tsx
@@ -1,5 +1,7 @@
+import { FontAwesome5 } from '@expo/vector-icons';
 import React, { useContext, useState } from 'react';
 import { View, Text, StyleSheet, Switch } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import MultiCitySelector from '../../shared/MultiCitySelector/MultiCitySelector';
 import { TravelPlannerWorkflowContext } from '../TravelPlannerWorkflow.state';
 
@@ -10,57 +12,66 @@ const CitySelectionWorkflow: React.FC = () => {
   const { departureCities, visitingCities, arrivalCities } = state;
 
   return (
-    <View style={{ backgroundColor: 'white', flex: 1, padding: 10 }}>
-      <Text style={styles.title}>Ótimo, então vamos lá!</Text>
-      <Text style={styles.highlightedTitle}>
-        Primeiramente, eu preciso saber quais são seus possíveis pontos de partida, quais cidades você deseja visitar e
-        os possíveis destinos finais.
-      </Text>
-      <Text style={styles.inputLabel}>Possíveis pontos de partida:</Text>
-      <MultiCitySelector
-        setCities={cities => {
-          dispatch({ type: 'setDepartureCities', cities: cities });
-          sameDepartureArrival && dispatch({ type: 'setArrivalCities', cities: cities });
-        }}
-        cities={departureCities}
-      />
-      <Text style={styles.inputLabel}>Cidades para visitar:</Text>
-      <MultiCitySelector
-        onAddCity={city =>
-          dispatch({
-            type: 'setVisitingCities',
-            cities: [...visitingCities, { city: city, minDays: 3, maxDays: 5 }]
-          })
-        }
-        onRemoveCity={city =>
-          dispatch({
-            type: 'setVisitingCities',
-            cities: state.visitingCities.filter(cityPeriod => cityPeriod.city.id !== city.id)
-          })
-        }
-        cities={visitingCities.map(cityStayPeriod => cityStayPeriod.city)}
-      />
-      <Text style={styles.inputLabel}>Possíveis destinos finais:</Text>
-      <MultiCitySelector
-        disabled={sameDepartureArrival}
-        setCities={cities => dispatch({ type: 'setArrivalCities', cities: cities })}
-        cities={sameDepartureArrival ? [] : arrivalCities}
-      />
-      <View style={{ flexDirection: 'row', marginTop: 5 }}>
-        <Switch
-          trackColor={{ false: '#767577', true: '#FC427B' }}
-          thumbColor={'#f4f3f4'}
-          onValueChange={value => {
-            value
-              ? dispatch({ type: 'setArrivalCities', cities: state.departureCities })
-              : dispatch({ type: 'setArrivalCities', cities: [] });
-            setSameDepartureArrival(value);
-          }}
-          value={sameDepartureArrival}
-        />
-        <Text style={{ flex: 1, marginLeft: 10, fontSize: 13 }}>
-          Usar os mesmos pontos de partida e destinos finais (ida e volta)
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={{ flex: 1, padding: 10 }}>
+        <Text style={styles.title}>Ótimo, então vamos lá!</Text>
+        <Text style={styles.highlightedTitle}>
+          Primeiramente, eu preciso saber quais são seus possíveis pontos de partida, quais cidades você deseja visitar
+          e os possíveis destinos finais.
         </Text>
+        <Text style={styles.inputLabel}>Possíveis pontos de partida:</Text>
+        <MultiCitySelector
+          setCities={cities => {
+            dispatch({ type: 'setDepartureCities', cities: cities });
+            sameDepartureArrival && dispatch({ type: 'setArrivalCities', cities: cities });
+          }}
+          cities={departureCities}
+        />
+        <Text style={styles.inputLabel}>Cidades para visitar:</Text>
+        <MultiCitySelector
+          onAddCity={city =>
+            dispatch({
+              type: 'setVisitingCities',
+              cities: [...visitingCities, { city: city, minDays: 3, maxDays: 5 }]
+            })
+          }
+          onRemoveCity={city =>
+            dispatch({
+              type: 'setVisitingCities',
+              cities: state.visitingCities.filter(cityPeriod => cityPeriod.city.id !== city.id)
+            })
+          }
+          cities={visitingCities.map(cityStayPeriod => cityStayPeriod.city)}
+        />
+        <Text style={styles.inputLabel}>Possíveis destinos finais:</Text>
+        <MultiCitySelector
+          disabled={sameDepartureArrival}
+          setCities={cities => dispatch({ type: 'setArrivalCities', cities: cities })}
+          cities={sameDepartureArrival ? [] : arrivalCities}
+        />
+        <View style={{ flexDirection: 'row', marginTop: 5 }}>
+          <Switch
+            trackColor={{ false: '#767577', true: '#FC427B' }}
+            thumbColor={'#f4f3f4'}
+            onValueChange={value => {
+              value
+                ? dispatch({ type: 'setArrivalCities', cities: state.departureCities })
+                : dispatch({ type: 'setArrivalCities', cities: [] });
+              setSameDepartureArrival(value);
+            }}
+            value={sameDepartureArrival}
+          />
+          <Text style={{ flex: 1, marginLeft: 10, fontSize: 13 }}>
+            Usar os mesmos pontos de partida e destinos finais (ida e volta)
+          </Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <View style={{ flex: 0.5 }}>
+          <TouchableOpacity style={{ paddingVertical: 20, alignItems: 'center' }}>
+            <FontAwesome5 name="chevron-right" size={24} color="#FC427B" />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
--- mobile/TravelPlannerWorkflow/NavigationTabBar/NavigationTabBar.tsx
+++ mobile/TravelPlannerWorkflow/NavigationTabBar/NavigationTabBar.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { TouchableOpacity, View, Text } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { WorkflowSection } from '../TravelPlannerWorkflow.d';
+import MaskedView from '@react-native-masked-view/masked-view';
 
 interface NavigationTabBarProps extends MaterialTopTabBarProps {
   currentWorkflowSection: WorkflowSection;
@@ -23,58 +24,70 @@ const NavigationTabBar: React.FC<NavigationTabBarProps> = ({
   };
 
   return (
-    <View style={{ flexDirection: 'row' }}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label = route.name;
+    <MaskedView
+      style={{ flexDirection: 'row', padding: 15 }}
+      maskElement={
+        <View style={{ flexDirection: 'row', backgroundColor: 'transparent' }}>
+          {state.routes.map((route, index) => {
+            const { options } = descriptors[route.key];
+            const label = route.name;
 
-        const isFocused = state.index === index;
+            const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true
-          });
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true
+              });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route.name);
+              }
+            };
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key
-          });
-        };
+            const onLongPress = () => {
+              navigation.emit({
+                type: 'tabLongPress',
+                target: route.key
+              });
+            };
 
-        const inputRange = state.routes.map((_, i) => i);
-        /*const opacity = Animated.interpolate(position, {
+            const inputRange = state.routes.map((_, i) => i);
+            /*const opacity = Animated.interpolate(position, {
           inputRange,
           outputRange: inputRange.map(i => (i === index ? 1 : 0))
         });*/
-        const isDisabled = index > currentWorkflowSection;
+            const isDisabled = index > currentWorkflowSection;
 
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            disabled={isDisabled}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{ flex: 1, alignItems: 'center', padding: 10 }}
-          >
-            <FontAwesome5
-              name={routeIconMap[route.name]}
-              color={isFocused ? '#FC427B' : isDisabled ? '#AAA' : '#333'}
-              size={28}
-            />
-          </TouchableOpacity>
-        );
-      })}
-    </View>
+            return (
+              <TouchableOpacity
+                accessibilityRole="button"
+                accessibilityState={isFocused ? { selected: true } : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                disabled={isDisabled}
+                onPress={onPress}
+                onLongPress={onLongPress}
+                style={{ flex: 1, alignItems: 'center', padding: 15 }}
+              >
+                <FontAwesome5
+                  name={routeIconMap[route.name]}
+                  color={isFocused ? '#FC427B' : isDisabled ? '#999' : '#7893d3'}
+                  size={28}
+                />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      }
+    >
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#324376' }} />
+      <FontAwesome5 name={'map-signs'} size={28} color="transparent" />
+      {/** Only here to give height to the masked view */}
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#F5DD90' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#F76C5E' }} />
+      <View style={{ flex: 1, height: '100%', backgroundColor: '#e1e1e1' }} />
+    </MaskedView>
   );
 };
 
